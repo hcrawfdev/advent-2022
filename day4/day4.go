78de5450d8ac3dabe2527b61479711a7157367b8
hcrawf <electronics.crawford@gmail.com>
2022-12-06 21:46:24 -0500

    updated git email..

--- day4/day4.go
+++ day4/day4.go
@@ -18,6 +18,7 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	var input []string
+
 	for scanner.Scan() {
 		//fmt.Println(scanner.Text())
 		input = append(input, scanner.Text())
